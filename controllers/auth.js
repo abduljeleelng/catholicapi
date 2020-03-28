55259fe2dabdd5695d5ea36da9368087100bb300
abduljeleelng <abduljeleelng@gmail.com>
2020-03-28 12:09:20 +0100

    fixed bug

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,7 +14,7 @@ exports.signup = async (req,res)=>{
         //res.json(user);
         const emailData = {
             from: "noreply@imcatholic.org",
-            to: "abduljeleelng@gmail.com",
+            to: user.email,
             subject: "Welcome to The word of Christ, I am Catholic",
             html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
             <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
