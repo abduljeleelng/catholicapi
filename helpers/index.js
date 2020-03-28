a9a53d840014fa4c363fa07481696a9177c97da2
abduljeleelng <abduljeleelng@gmail.com>
2020-03-28 10:33:38 +0100

    mail configuration

--- helpers/index.js
+++ helpers/index.js
@@ -1,12 +1,12 @@
 const nodeMailer = require("nodemailer");
 
-const defaultEmailData = { from: "noreply@iamctholic.org" };
+//const defaultEmailData = { from: "noreply@iamctholic.org" };
 
 exports.sendEmail = emailData => {
     const transporter = nodeMailer.createTransport({
         host: 'mail.unglobal.ng ',
-        port: 465,
-        secure: true, // true for 465, false for other ports
+        port: 587,
+        secure: false, // true for 465, false for other ports
         auth: {
           user: 'developer@unglobal.ng', // your domain email address
           pass: 'Unitech2020' // your password
